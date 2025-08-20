9002013b95e9ca7ae01fc0e7e740eb03719f20ec
Kari Silvennoinen <kari@kalifi.org>
2025-08-20 22:30:23 +0300

    add version

--- cmd/weather/main.go
+++ cmd/weather/main.go
@@ -7,10 +7,14 @@ import (
 	"github.com/kari/fmi"
 )
 
+var Version = "development"
+
 func main() {
-	if len(os.Args) > 1 {
-		fmt.Println(fmi.Weather(os.Args[1]))
-	} else {
+	if len(os.Args) <= 1 {
 		fmt.Println("Usage:", os.Args[0], "<paikka>")
+	} else if os.Args[1] == "version" {
+		fmt.Println("Version:", Version)
+	} else {
+		fmt.Println(fmi.Weather(os.Args[1]))
 	}
 }
